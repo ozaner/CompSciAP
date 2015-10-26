be6820c4e5812449492e0a76cf3d6d4932387648
ozanerhansha <weegeesixtyfour@gmail.com>
2015-10-26 17:48:27 -0400

    ArrayTester Revision 1
    Finished the project, will see if it is up to spec. according to Dr.
    Jones

--- src/unit1/ArrayTester.java
+++ src/unit1/ArrayTester.java
@@ -3,11 +3,20 @@ package unit1;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Scanner;
 
+/**
+ * This a "Self-Test" on using Arrays.<br><br>
+ * 
+ * AP Computer Science<br>
+ * Dr. Jones<br>
+ * October 27, 2015
+ * @author Ozaner Hansha
+ */
 public class ArrayTester
 {
 	/**
-	 * An array of Strings of the 7 days of the week.
+	 * An array of Strings of the 7 days of the week. Starting at Monday.
 	 */
 	public static final String[] DAYS_OF_THE_WEEK =
 		{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
@@ -23,64 +32,95 @@ public class ArrayTester
 	
 	/**
 	 * This method calls the {@link Collections#shuffle(java.util.List)}
-	 * @param copy - An Array of Strings to be shuffled.
+	 * @param s - An Array of Strings to be shuffled.
 	 */
 	private static void shuffleStringArray(String[] s)
 	{
 		Collections.shuffle(Arrays.asList(s));
 	}
 
+	/**
+	 * This method randomizes a given array of Strings using a simple algorithm.
+	 * @param s - An Array of Strings to be shuffled.
+	 */
 	private static void shuffleStringArray2(String[] s)
 	{
-		ArrayList<String> temp = new ArrayList<String>(Arrays.asList(s));
-		for(int x = 0; x > s.length; x++)
+		ArrayList<String> temp = new ArrayList<String>(Arrays.asList(s)); //the rest of the non randomized array aka 'temp'.
+		for(int x = 0; x < s.length; x++) //For every index of the given array (s).
 		{
-			int rand = (int)(Math.random()*temp.size());
-			s[x] = temp.get(rand);
-			temp.remove(rand);
+			int rand = (int)(Math.random()*temp.size()); //Creates a random int between 0 and the size of temp.
+			s[x] = temp.get(rand); //Sets the index of the given array to a random variable in the rest temp.
+			temp.remove(rand); //Removes the string at that same index to prevent duplicates.
 		}
 	}
 	
 	/**
 	 * Calls the {@link Arrays#sort(Object[])} method on an array of Strings.
-	 * @param copy - The array of Strings to sort.
+	 * @param s - The array of Strings to sort.
 	 */
 	private static void sortStringArray(String[] s)
 	{
 		Arrays.sort(s);
 	}
 	
+	/**
+	 * Calls the {@link Collections#reverse(java.util.List)} method on an array of Strings.
+	 * @param s - The array of Strings to reverse.
+	 */
 	private static void reverseStringArray(String[] s)
 	{
-		String[] temp = new String[s.length]; //Makes a temporary array to store the values.
-		for(int c = 0; c < s.length; c++)
-			temp[c] = s[s.length-1-c];
-		System.arraycopy(temp, 0, s, 0, s.length); //Copies the contents of temp top
+//	If I did have to create a method for this:
+/*
+ *		String[] temp = new String[s.length]; //Makes a temporary array to store the values.
+ *		for(int c = 0; c < s.length; c++)
+ *			temp[c] = s[s.length-1-c];
+ *
+ * 		System.arraycopy(temp, 0, s, 0, s.length); //Copies the contents of temp top
+ */
+		Collections.reverse(Arrays.asList(s));	
 	}
 	
 	public static void main(String[] args)
 	{
-		String[] copy; 
-		System.out.printf("getRandomDay() returns %s\n", getRandomDay());
-		
-		copy = Arrays.copyOf(DAYS_OF_THE_WEEK, DAYS_OF_THE_WEEK.length); 
-		sortStringArray(copy);
-		System.out.printf("sortStringArray(%s) yields\n\t%s\n", 
-				Arrays.toString(DAYS_OF_THE_WEEK), Arrays.toString(copy));
-		
-		copy = Arrays.copyOf(DAYS_OF_THE_WEEK, DAYS_OF_THE_WEEK.length);
-		shuffleStringArray(copy);
-		System.out.printf("shuffleStringArray(%s) yields\n\t%s\n",
-				Arrays.toString(DAYS_OF_THE_WEEK), Arrays.toString(copy));
-		
-		copy = Arrays.copyOf(DAYS_OF_THE_WEEK, DAYS_OF_THE_WEEK.length);
-		reverseStringArray(copy);
-		System.out.printf("reverseStringArray(%s) yields\n\t%s\n",
-				Arrays.toString(DAYS_OF_THE_WEEK), Arrays.toString(copy));
-		
-		copy = Arrays.copyOf(DAYS_OF_THE_WEEK, DAYS_OF_THE_WEEK.length); 
-		shuffleStringArray2(copy);
-		System.out.printf("shuffleStringArray2(%s) yields \n\t%s\n", 
-				Arrays.toString(DAYS_OF_THE_WEEK), Arrays.toString(copy));
+		String[] copy;
+		Scanner in = new Scanner(System.in);
+	
+		while(true)
+		{
+			copy = Arrays.copyOf(DAYS_OF_THE_WEEK, DAYS_OF_THE_WEEK.length); //Resets the copy array
+			System.out.printf("Please enter either:\n•day\t•shuffle\n•sort\t•reverse\n•exit\t•shuffle2\n> ");
+			switch(in.nextLine())
+			{
+			case("day"):
+				System.out.printf("getRandomDay() returns %s\n", getRandomDay());
+				break;
+			case("sort"):
+				sortStringArray(copy);
+				System.out.printf("sortStringArray(%s) yields\n\t%s\n", 
+					Arrays.toString(DAYS_OF_THE_WEEK), Arrays.toString(copy));
+				break;
+			case("shuffle"):
+				shuffleStringArray(copy);
+				System.out.printf("shuffleStringArray(%s) yields\n\t%s\n",
+					Arrays.toString(DAYS_OF_THE_WEEK), Arrays.toString(copy));
+				break;
+			case("reverse"):
+				reverseStringArray(copy);
+				System.out.printf("reverseStringArray(%s) yields\n\t%s\n",
+						Arrays.toString(DAYS_OF_THE_WEEK), Arrays.toString(copy));
+				break;
+			case("shuffle2"): 
+				shuffleStringArray2(copy);
+				System.out.printf("shuffleStringArray2(%s) yields \n\t%s\n", 
+						Arrays.toString(DAYS_OF_THE_WEEK), Arrays.toString(copy));
+				break;
+			case("exit"):
+				System.out.println("Terminating Program... Goodbye!");
+				in.close();
+				System.exit(0);
+			default:
+				System.out.println("[ArrayTester] Invalid input, please try again.");
+			}
+		}
 	}
 }
