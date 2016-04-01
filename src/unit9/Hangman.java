e516d102fdb389624935724d41578de49d543867
ozanerhansha <ozanerhansha@CHS-A104-DSK-01.CHS.chatham-nj.org>
2016-04-01 13:29:33 -0400

    hangman 1

--- src/unit9/Hangman.java
+++ src/unit9/Hangman.java
@@ -1,7 +1,72 @@
 package unit9;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+import acm.graphics.GLine;
+import acm.graphics.GPoint;
 import acm.program.GraphicsProgram;
 
 public class Hangman extends GraphicsProgram implements HangmanView {
+
+	public static final Dimension WINDOW_SIZE = new Dimension(1000,800);
 	
+	public static final Dimension GRAPHICS_SIZE = new Dimension(200,300);
+	
+	public static final GPoint GRAPHICS_COORDS = new GPoint(250,250);
+	
+	public static final GPoint WORD_COORDS = new GPoint(350,0);
+	
+	public static final int LINE_Y = 600;
+
+	public static final int LINE_GAP = 40;
+	
+	public static final int LINE_OFFSET = 100;
+	
+	public static final int LINE_SIZE_FACTOR = 90;
+	
+	private HangmanModel model = new HangmanModel(this);
+	
+	private HangmanGraphics graphics = 
+			new HangmanGraphics(GRAPHICS_SIZE.getWidth(),GRAPHICS_SIZE.getHeight());
+	
+	@Override
+	public void init() {
+		setSize(WINDOW_SIZE);
+		setBackground(Color.DARK_GRAY);
+		graphics.drawNextPart();
+		add(graphics,GRAPHICS_COORDS);
+		gameStartNotification("algorithm");
+	}
+
+	@Override
+	public void gameStartNotification(String word) {
+		for(int x = 0; x < word.length(); x++) {
+			System.out.println(x);
+			add(new GLine(x*LINE_SIZE_FACTOR + LINE_OFFSET,LINE_Y,x*LINE_SIZE_FACTOR + LINE_OFFSET + LINE_GAP,LINE_Y));
+		}
+	}
+
+	@Override
+	public void gameOverNotification() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void correctNotification(char c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void incorrectNotification(char c) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		Hangman e = new Hangman();
+		e.start();
+	}
 }
--- src/unit9/HangmanModel.java
+++ src/unit9/HangmanModel.java
@@ -1,17 +1,46 @@
 package unit9;
 
 public class HangmanModel {
-	public static char[] ALPHABET = {
-		'a','b','c','d','e','f','g','h','i','j','k','l','m',
-		'n','o','p','q','r','s','t','u','v','w','x','y','z'
-	};
 	
+	/**
+	 * Regular expression for Alphabetic characters.
+	 */
+	public static String VALID_CHARACTERS = "[a-zA-Z]";
+	
+	/**
+	 * Reference to corresponding {@link HangmanView}.
+	 */
 	private HangmanView view;
+	
+	/**
+	 * Player's score this round.
+	 */
 	private int score;
+	
+	/**
+	 * The current word in play.
+	 */
 	private String currentWord;
+	
+	/**
+	 * An array of guessed letters.
+	 */
 	private char[] guessedLetters;
 	
+	/**
+	 * Constructor for {@link HangmanModel}.
+	 * @param view - A corresponding {@link HangmanView}.
+	 */
 	public HangmanModel(HangmanView view) {
 		this.view = view;
+	}
+	
+	/**
+	 * Checks whether a guess is valid (i.e alphabetic).
+	 * @param c -  a character.
+	 * @return The validity of a guess.
+	 */
+	public boolean isValidGuess(char c) {
+		return (""+c).matches(VALID_CHARACTERS);
 	}
 }
